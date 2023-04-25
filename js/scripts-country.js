d97ece6a3a307bb21008cff9acbf789573bab32a
Vincent Turmine <vincent.turmine@gmail.com>
2023-04-25 16:43:11 +0200

    update Country page

---
+++ js/scripts-country.js
@@ -0,0 +1,346 @@
+
+const dataURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQbooW7TmLrMZ8QNc4IlGq4mKaZQflviQ1WNPzeMHLemb8Nl5QdsDQnR5TnWHeNOzsFY479CV-tHbNY/pub?gid=1401474139&single=true&output=csv&force=on";
+let CTI=[];
+const countryISO = 'ZMB';
+var CTIdata = [];
+
+
+$(document).ready(function() {
+    function getData() {
+        Promise.all([
+            d3.csv(dataURL),
+        ]).then(function(data) {
+            CTI=data[0].filter(d => { return d.ISO == countryISO; });
+            CTIdata = [parseInt(CTI[0]['Competency']), parseInt(CTI[0]['Value']), parseInt(CTI[0]['Overall'])];
+            console.log(CTI);
+            generateRadialChart(CTIdata);    
+        }); // then
+       
+    } // getData
+    getData();
+});
+
+console.log(CTI);
+console.log(CTIdata);
+//CTIdata = [55,77,99];
+
+
+window.Apex = {
+  chart: {
+    foreColor: '#ccc',
+    toolbar: {
+      show: true
+    },
+  },
+  stroke: {
+    width: 3
+  },
+  dataLabels: {
+    enabled: true
+  },
+  tooltip: {
+    theme: 'dark'
+  },
+  grid: {
+    borderColor: "#535A6C",
+    xaxis: {
+      lines: {
+        show: true
+      }
+    }
+  }
+};
+
+
+
+
+function generateRadialChart(data){
+    console.log(data);
+    var optionsCircle4 = {
+        chart: {
+          type: 'radialBar',
+          height: 350,
+          width: '100%',
+        },
+        plotOptions: {
+          radialBar: {
+            size: undefined,
+            inverseOrder: true,
+            hollow: {
+              margin: 10,
+              size: '35%',
+              background: 'transparent', //'#FFF', 
+      
+            },
+            track: {
+              show: false,
+            },
+            startAngle: -180,
+            endAngle: 180
+      
+          },
+        },
+        stroke: {
+          lineCap: 'round'
+        },
+        series: data,
+        labels: ['Competency', 'Value', 'Trust Index'],
+        colors: ['#0080ff', '#5F61B5', '#FF0000'],
+        legend: {
+          show: true,
+          floating: true,
+          position: 'bottom',
+          offsetX: 0,
+          offsetY: 0
+        },
+      }
+      
+      var chartCircle4 = new ApexCharts(document.querySelector('#radialBarBottom'), optionsCircle4);
+      chartCircle4.render();
+        
+}
+
+var optionsBar1 = {
+  colors:['#bf0000', '#ff0000','#6B66B7','#090088','#d6d6d6'],
+  chart: {
+    height: 380,
+    width:'100%',
+    type: 'bar',
+    stacked: true,
+    stackType: '100%'
+  },
+  plotOptions: {
+    bar: {
+      columnWidth: '30%',
+      horizontal: true,
+    },
+  },
+
+  series: [{
+    name: 'Not at all',
+    data: [1, 1.2, 1.9, 1.5, 1.9, 1.7, 4.4, 2.3]
+  }, {
+    name: 'Not so much',
+    data: [2.7, 4, 3.3, 2.5, 3.7, 3.7, 6.7, 3.7]
+  }, {
+    name: 'Mostly yes',
+    data: [7.1, 8.3, 8.9, 10.2, 10.2, 11.0, 5.8, 11.9]
+  }, {
+    name: 'Yes completely',
+    data: [85.9, 84.8, 84.0, 83.6, 81.3, 81.3, 79, 78.4]
+  }, {
+    name: 'Don’t know',
+    data: [3.3, 1.7, 1.9, 2.3, 2.9, 2.3, 4.2, 3.7]
+  }
+],
+  xaxis: {
+    categories: ['Goodwill', 'Fairness', 'Inclusiveness', 'No discrimination', 'Participation', 'Integrity', 'Transparency','Neutrality'],
+  },
+
+  yaxis: {
+   
+    labels: {
+     style: {
+        colors: [],
+        fontSize: '15px',
+        fontFamily: 'Helvetica, Arial, sans-serif',
+        fontWeight: 400,
+        cssClass: 'apexcharts-yaxis-label',
+        },
+    },
+  },
+
+  fill: {
+    opacity: 1
+  },
+  grid: {
+    show: false,
+    },
+   title: {
+        text: 'Perception of Value',
+        align: 'center',
+        margin: 10,
+        offsetX: 50,
+        style: {
+            fontSize:  '14px',
+            fontWeight:  'bold',
+            fontFamily:  "Open Sans",
+            color:  '#263238'
+          },
+    },
+    legend: { 
+      offsetX: 65,
+    }
+}
+
+var chartBar1 = new ApexCharts(
+  document.querySelector("#barchart-value"),
+  optionsBar1
+);
+
+
+chartBar1.render();
+
+
+var optionsBar2 = {
+    colors:['#bf0000', '#ff0000','#6B66B7','#090088','#d6d6d6'],
+    chart: {
+      height: 380,
+      width:'100%',
+      type: 'bar',
+      stacked: true,
+      stackType: '100%'
+    },
+    plotOptions: {
+      bar: {
+        columnWidth: '30%',
+        horizontal: true,
+      },
+    },
+  
+    series: [{
+      name: 'Not at all',
+      data: [1, 1.2, 1.9, 1.5, 1.9, 1.7]
+    }, {
+      name: 'Not so much',
+      data: [2.7, 4, 3.3, 2.5, 3.7, 3.7]
+    }, {
+      name: 'Mostly yes',
+      data: [7.1, 8.3, 8.9, 10.2, 10.2, 11.0]
+    }, {
+      name: 'Yes completely',
+      data: [85.9, 84.8, 84.0, 83.6, 81.3, 81.3]
+    }, {
+      name: 'Don’t know',
+      data: [3.3, 1.7, 1.9, 2.3, 2.9, 2.3]
+    }
+  ],
+    xaxis: {
+      categories: ['Capability', 'Responsiveness', 'Knowledge', 'Approachability', 'Effectiveness', 'Relevance'],
+    },
+  
+    yaxis: {
+     
+      labels: {
+       style: {
+          colors: [],
+          fontSize: '15px',
+          fontFamily: 'Helvetica, Arial, sans-serif',
+          fontWeight: 400,
+          cssClass: 'apexcharts-yaxis-label',
+          },
+      },
+    },
+  
+    fill: {
+      opacity: 1
+    },
+    grid: {
+      show: false,
+      },
+     title: {
+          text: 'Perception of Competencies',
+          align: 'center',
+          margin: 10,
+          offsetX: 50,
+          style: {
+              fontSize:  '14px',
+              fontWeight:  'bold',
+              fontFamily:  "Open Sans",
+              color:  '#263238'
+            },
+      },
+      legend: { 
+        offsetX: 65,
+      }
+  }
+  
+  var chartBar2 = new ApexCharts(
+    document.querySelector("#barchart-comp"),
+    optionsBar2
+  );
+  
+  
+  chartBar2.render();
+
+
+
+      
+var optionsDist = {
+    series: [{
+    name: 'Males',
+    data: [14.69, 12.95, 16.96, 11.08
+    ]
+  },
+  {
+    name: 'Females',
+    data: [-12.28, -9.35, -15.49, -7.21
+    ]
+  }
+  ],
+    chart: {
+    type: 'bar',
+    height: 300,
+    stacked: true
+  },
+  colors: ['#008FFB', '#FF4560'],
+  plotOptions: {
+    bar: {
+      horizontal: true,
+      barHeight: '80%',
+    },
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    width: 3,
+    colors: ["#12284C"]
+  },
+  
+  grid: {
+    xaxis: {
+      lines: {
+        show: false
+      }
+    }
+  },
+  yaxis: {
+    min: -20,
+    max: 20,
+    title: {
+      // text: 'Age',
+    },
+  },
+  tooltip: {
+    shared: false,
+    x: {
+      formatter: function (val) {
+        return val
+      }
+    },
+    y: {
+      formatter: function (val) {
+        return Math.abs(val) + "%"
+      }
+    }
+  },
+  title: {
+    text: 'Respondents'
+  },
+  xaxis: {
+    categories: ['45+', '35-44', '25-34', '18-24'
+    ],
+    title: {
+      text: 'Percent'
+    },
+    labels: {
+      formatter: function (val) {
+        return Math.abs(Math.round(val)) + "%"
+      }
+    }
+  },
+  };
+
+  var chartDist = new ApexCharts(document.querySelector("#chartDist"), optionsDist);
+  chartDist.render();
