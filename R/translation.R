71971f6db7c780ee809d3ebb9b26b409a6689a82
Vincent Turmine <vincent.turmine@gmail.com>
2026-08-10 14:20:38 +0200

    Update translation.R

--- R/translation.R
+++ R/translation.R
@@ -58,7 +58,7 @@ repo_path <- function(repo, ...) {
 }
 
 load_translation_dictionary <- function(
-  file = repo_path(repo, "R", "translation.csv"),
+  file = repo_path(repo, "R", "translations.csv"),
   languages = c("EN", "FR", "ES"),
   fallback_language = "EN"
 ) {
